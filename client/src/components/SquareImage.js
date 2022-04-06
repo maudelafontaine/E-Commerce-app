9cd803a59ba5420e513e6d351280fd3a8aeab0ff
csimpson1 <csimpson.2@gmail.com>
2022-04-05 21:05:18 -0400

    Total Function, Square Image component
    What changed?
    Added in function to cart context to get total price of items
    Created small component for displaying square images
    
    Why?
    Needed to display total in card, figured we'd have square images in a few places
    
    References

---
+++ client/src/components/SquareImage.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const SquareImage = styled.img`
+    width: ${props => props.size};
+    height: ${props => props.size};
+`;
+
+export default SquareImage;
